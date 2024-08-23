51eec181346b4cc579103ecd1fade92a586fa855
Zerfa <zerfa@naver.com>
2024-08-23 19:08:28 +0900

    green 0823 v3

--- springwork/blog/src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ springwork/blog/src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -108,7 +108,11 @@ public class BoardController {
         // request.setAttribute("model", board);
         //boolean 값을 하나 만들었다. -> 게시글 상세보기에서 권한확인을 위해 수정/삭제버튼을 이 값을 일단 넣어둠
         //request.setAttribute("isOwner", false);
+
         User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            sessionUser = User.builder().username("비회원").build();
+        }
         BoardResponse.DetailDTOV2 detailDTOV2 = boardService.상세보기(id, sessionUser);
         request.setAttribute("model", detailDTOV2);
 
